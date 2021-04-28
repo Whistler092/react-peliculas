75fb9c2e44b3895e491a3819198fd756fdd5dd4c
Ramiro Andres Bedoya <iamramiroo@gmail.com>
2021-04-27 21:10:13 -0500

    remove unused elements

--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -1,5 +1,3 @@
-import { ReactElement } from "react";
-
 export default function Button(props: buttonProps) {
   return (
     <button type="button" onClick={props.onClick} className="btn btn-primary">
