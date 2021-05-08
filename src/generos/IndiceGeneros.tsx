fcf91b4cbaa5f728d87dc07b7ab5cd4200d6ed6c
Ramiro Andres Bedoya <iamramiroo@gmail.com>
2021-05-08 14:59:03 -0500

    Enviroment vars

--- src/generos/IndiceGeneros.tsx
+++ src/generos/IndiceGeneros.tsx
@@ -1,6 +1,18 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { urlGeneros } from "../utils/endpoints";
+import { generoDTO } from "./generos.model";
 
 export default function IndiceGeneros() {
+
+  useEffect(() => {
+    axios.get(urlGeneros)
+    .then((respuesta: AxiosResponse<generoDTO>) => {
+      console.log('data', respuesta.data);
+    })
+  }, []);
+
   return (
     <>
       <h3>Indice Generos</h3>
